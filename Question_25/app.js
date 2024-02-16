2c021b057f43001ee5c72a288369bc17c2b3b9cf
anusmemon226 <anusm226@gmail.com>
2024-02-17 00:19:43 +0500

    Solved Question 25 ready for deployment

---
+++ Question_25/app.js
@@ -0,0 +1,10 @@
+var alien_color = "green";
+if (alien_color == 'green') {
+    console.log("Player just earned 5 points");
+}
+if (alien_color == 'yellow') {
+    console.log("Player just earned 5 points");
+}
+else {
+    console.log("");
+}
