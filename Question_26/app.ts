71eba36ac30ce997a33adee6dd775670fb8ec739
anusmemon226 <anusm226@gmail.com>
2024-02-17 00:23:02 +0500

    Solved Question 26 ready for deployment

---
+++ Question_26/app.ts
@@ -0,0 +1,16 @@
+// will run if part 
+let alien_color1 = "green"
+if(alien_color1 == 'green'){
+    console.log("The player just earned 5 points for shooting the alien.")
+}else{
+    console.log("The player just earned 10 points for shooting the alien.")
+}
+
+
+// will run else part 
+let alien_color2 = "yellow"
+if(alien_color2 == 'green'){
+    console.log("The player just earned 5 points for shooting the alien.")
+}else{
+    console.log("The player just earned 10 points for shooting the alien.")
+}
