bd4568b5ce3f1034c6f303ad1648d4f2469f2d9d
anusmemon226 <anusm226@gmail.com>
2024-02-16 23:27:53 +0500

    Solved Question 23 ready for deployment

---
+++ Question_23/app.ts
@@ -0,0 +1,20 @@
+let car1 = "sabaru"
+console.log(car1=="sabaru"?true:false)
+let car2 = "honda"
+console.log(car2=="sabaru"?true:false)
+let bike1 = "Super star"
+console.log(bike1=="unique"?true:false)
+let bike2 = "unique"
+console.log(bike2=="unique"?true:false)
+let flower1 = "rose"
+console.log(flower1=="rose"?true:false)
+let flower2 = "sunflower"
+console.log(flower2=="rose"?true:false)
+let mobile1 = "Infinix"
+console.log(mobile1=="Infinix"?true:false)
+let mobile2 = "Apple"
+console.log(mobile2=="Infinix"?true:false)
+let watch1 = "rolex"
+console.log(watch1=="rolex"?true:false)
+let watch2 = "rolex"
+console.log(watch2=="apple watch"?true:false)
