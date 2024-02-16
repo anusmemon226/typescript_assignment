5fabea4c02526bae77eeb8c0ae2f1e19f9ff60ea
anusmemon226 <anusm226@gmail.com>
2024-02-16 22:58:23 +0500

    Solved Question 20 ready for deployment

---
+++ Question_20/app.js
@@ -0,0 +1,5 @@
+var countries = ["Pakistan", "USA", "Australia", "India", "France", "United Kingdom", "Canada", "Westindies", "Saudia Arabia", "Switzerland", "Bangladesh"];
+console.log("List of Countries");
+for (var country in countries) {
+    console.log(countries[country]);
+}
---
+++ Question_20/app.ts
@@ -0,0 +1,5 @@
+let countries = ["Pakistan","USA","Australia","India","France","United Kingdom","Canada","Westindies","Saudia Arabia","Switzerland","Bangladesh"]
+console.log("List of Countries")
+for(let country in countries){
+    console.log(countries[country])
+}
