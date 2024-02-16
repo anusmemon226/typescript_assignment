e940853255f95087a3f2c31959b23a6cfb20caab
anusmemon226 <anusm226@gmail.com>
2024-02-16 20:33:17 +0500

    Solved Question 13 Ready for deployment

---
+++ Question_13/app.js
@@ -0,0 +1,5 @@
+var favTransport = ["Bike", "Car", "Airplane", "Train"];
+console.log("I would like to own a " + favTransport[0]);
+console.log("I've always dreamed of driving a " + favTransport[1]);
+console.log("One day, I hope to fly in an " + favTransport[2]);
+console.log("I wouldn't mind taking a trip on a " + favTransport[3]);
