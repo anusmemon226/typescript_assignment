858a548c11004d03bea57ac4974b42579d7a3ffb
anusmemon226 <anusm226@gmail.com>
2024-02-16 18:36:07 +0500

    Solved Question 5 ready for deployment

---
+++ Question_05/app.js
@@ -0,0 +1,3 @@
+var famous_person = "Quaid e Azam Muhammad Ali Jinnah";
+var message = "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one men.";
+console.log("".concat(famous_person, " once said, \"").concat(message, "\""));
---
+++ Question_05/app.ts
@@ -0,0 +1,4 @@
+let famous_person = "Quaid e Azam Muhammad Ali Jinnah"
+let message = "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one men."
+
+console.log(`${famous_person} once said, "${message}"`)
