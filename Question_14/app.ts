36ed00c77909e827a8867fb7b90d61850b617468
anusmemon226 <anusm226@gmail.com>
2024-02-16 20:43:54 +0500

    Solved Question 14 ready for deployment

---
+++ Question_14/app.ts
@@ -0,0 +1,5 @@
+let guestList = ["Mr Ahmed","Mr Shahid Afridi","Mr Imran Khan"]
+
+for(let i=0;i<guestList.length;i++){
+    console.log(`${guestList[i]}! you are invited for dinner at my farm house.`)
+}
