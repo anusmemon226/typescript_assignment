af09115911735e5497f0377158f1d15c414242cb
anusmemon226 <anusm226@gmail.com>
2024-02-16 23:05:12 +0500

    Solved Question 21 ready for deployment

---
+++ Question_21/app.js
@@ -0,0 +1,12 @@
+var obj = {
+    name: "Muhammad Anus",
+    age: 22,
+    email: "anusm226@gmail.com",
+    phone: "03302626644",
+    country: "Pakistan",
+    city: "Karachi",
+    state: "Sindh"
+};
+for (var items in obj) {
+    console.log(items + " : " + obj[items]);
+}
---
+++ Question_21/app.ts
@@ -0,0 +1,12 @@
+let obj = {
+    name : "Muhammad Anus",
+    age : 22,
+    email : "anusm226@gmail.com",
+    phone : "03302626644",
+    country : "Pakistan",
+    city : "Karachi",
+    state : "Sindh"
+}
+for(let items in obj){
+    console.log(items+" : "+obj[items])
+}
