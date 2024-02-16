5c0af447bd1d626b05994cd938d1faeec9d68da9
anusmemon226 <anusm226@gmail.com>
2024-02-17 00:37:40 +0500

    Solved Question 29 ready for deployment

---
+++ Question_29/app.js
@@ -0,0 +1,18 @@
+var favorite_fruits = ["Bananas", "Apple", "Orange"];
+for (var fav_fruit in favorite_fruits) {
+    if (favorite_fruits[fav_fruit] == "Bananas") {
+        console.log("You really like ".concat(favorite_fruits[fav_fruit]));
+    }
+    if (favorite_fruits[fav_fruit] == "Apple") {
+        console.log("You really like ".concat(favorite_fruits[fav_fruit]));
+    }
+    if (favorite_fruits[fav_fruit] == "Grapes") {
+        console.log("You really like ".concat(favorite_fruits[fav_fruit]));
+    }
+    if (favorite_fruits[fav_fruit] == "Orange") {
+        console.log("You really like ".concat(favorite_fruits[fav_fruit]));
+    }
+    if (favorite_fruits[fav_fruit] == "Lemon") {
+        console.log("You really like ".concat(favorite_fruits[fav_fruit]));
+    }
+}
---
+++ Question_29/app.ts
@@ -0,0 +1,19 @@
+let favorite_fruits = ["Bananas","Apple","Orange"]
+
+for(let fav_fruit in favorite_fruits){
+    if(favorite_fruits[fav_fruit]=="Bananas"){
+        console.log(`You really like ${favorite_fruits[fav_fruit]}`)
+    }
+    if(favorite_fruits[fav_fruit]=="Apple"){
+        console.log(`You really like ${favorite_fruits[fav_fruit]}`)
+    }
+    if(favorite_fruits[fav_fruit]=="Grapes"){
+        console.log(`You really like ${favorite_fruits[fav_fruit]}`)
+    }
+    if(favorite_fruits[fav_fruit]=="Orange"){
+        console.log(`You really like ${favorite_fruits[fav_fruit]}`)
+    }
+    if(favorite_fruits[fav_fruit]=="Lemon"){
+        console.log(`You really like ${favorite_fruits[fav_fruit]}`)
+    }
+}
