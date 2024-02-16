3cb74724802cd44c4c3e8b996f92a5299422fe8d
anusmemon226 <anusm226@gmail.com>
2024-02-16 20:10:34 +0500

    Solved Question 12 ready for deployment

---
+++ Question_12/app.js
@@ -0,0 +1,4 @@
+var names = ["Anas", "Bilal", "Ahmed", "Kamran"];
+for (var i = 0; i < names.length; i++) {
+    console.log("How are you ".concat(names[i], " ? Hope you will be fine."));
+}
---
+++ Question_12/app.ts
@@ -0,0 +1,5 @@
+let names = ["Anas","Bilal","Ahmed","Kamran"]
+
+for (let i=0;i<names.length;i++){
+    console.log(`How are you ${names[i]} ? Hope you will be fine.`)
+}
