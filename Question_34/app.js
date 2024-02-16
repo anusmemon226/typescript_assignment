62a46614a970216b20eb7d4027730a0cc6c17461
anusmemon226 <anusm226@gmail.com>
2024-02-17 01:42:04 +0500

    Solved Question 34 ready for deployment

---
+++ Question_34/app.js
@@ -0,0 +1,15 @@
+var pizzas = ["Pepporoni", "Cheesy", "BBQ Chicken"];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log(pizza);
+}
+// Modified
+for (var _a = 0, pizzas_2 = pizzas; _a < pizzas_2.length; _a++) {
+    var pizza = pizzas_2[_a];
+    console.log("I like ".concat(pizza, " pizza"));
+}
+console.log("I really like pizza especially of these kinds : ");
+for (var _b = 0, pizzas_3 = pizzas; _b < pizzas_3.length; _b++) {
+    var pizza = pizzas_3[_b];
+    console.log(pizza);
+}
---
+++ Question_34/app.ts
@@ -0,0 +1,14 @@
+let pizzas = ["Pepporoni","Cheesy","BBQ Chicken"]
+for(let pizza of pizzas){
+    console.log(pizza)
+}
+
+// Modified
+for(let pizza of pizzas){
+    console.log(`I like ${pizza} pizza`)
+}
+
+console.log("I really like pizza especially of these kinds : ")
+for(let pizza of pizzas){
+    console.log(pizza)
+}
