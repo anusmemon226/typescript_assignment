ffc437c1d012393d413b8d61cb0be719d2ae7a0f
anusmemon226 <anusm226@gmail.com>
2024-02-17 02:00:16 +0500

    Solved Question 36 ready for deployment

---
+++ Question_36/app.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("Size of shirt : ".concat(size, "\nMessage : ").concat(message));
+}
+make_shirt("large", "Think Positive Do Your Best");
---
+++ Question_36/app.ts
@@ -0,0 +1,4 @@
+function make_shirt(size,message){
+    console.log(`Size of shirt : ${size}\nMessage : ${message}`)
+}
+make_shirt("large","Think Positive Do Your Best")
